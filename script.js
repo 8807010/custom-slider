89076d02eca1205957525fd8f2e329ca0aabe223
Artiom_Sokolov <sokolova-net@mail.ru>
2022-12-14 22:18:28 +0300

    slider

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+  let counter = 1;
+
+  setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+      counter = 1;
+    }
+  }, 5000);
+};
+
